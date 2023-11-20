cd0450a8ee82aac1025079f8ae1a32eeae442e56
anithalder <halderanit07@gmail.com>
2023-11-21 00:12:06 +0530

    update

---
+++ src/product.java
@@ -0,0 +1,17 @@
+class product {
+    long productId;
+    String productName;
+    double price;
+    int quantity;
+
+    product(long id, String name, double price, int quantity){
+        this.productId = id;
+        this.productName = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    product(){
+        
+    }
+}

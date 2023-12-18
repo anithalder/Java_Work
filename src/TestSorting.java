655bf4a78f1c6da800ee2ed609bf5b077efd1f36
anithalder <halderanit07@gmail.com>
2023-12-18 23:03:28 +0530

    updated

--- src/TestSorting.java
+++ src/TestSorting.java
@@ -3,6 +3,6 @@ import sorting.*;
 class TestSorting {
     public static void main(String[] args) {
         int[] arr = {2,4,1,7,3,5,8,6,9};
-        PrintArray.printArray(BubbleSort.sort(arr));
+        PrintArray.printArray(QuickSort.sort(arr));
     }
 }
